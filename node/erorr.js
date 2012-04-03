18d9db9b7aa9a371920fd80ed5dd44d312cda0b6
Leigh Pauls <leigh.pauls@gmail.com>
2012-04-03 01:06:12 -0400

    added support for POST calls

--- node/erorr.js
+++ node/erorr.js
@@ -1,36 +1,51 @@
-var app = require( "express" ).createServer();
+var express = require( "express" );
+var app = express.createServer();
 
-app.get( "/", function( req, res ) {
+app.use( express.bodyParser() );
 
-	// get the error code
-	var errorCode = parseInt( req.query.error, 10 );
-	if ( !errorCode ) {
-		// no code, return a splash page
-		// TODO: make this not a piece of crap
-		res.send( "Use thisserver.com/?error=XYZ, where XYZ is any valid server reponse code, and it will be returned\n" );
-		return;
+// makes a handler, tailor-made for get or post queries
+function handlerMaker( type ) {
+	var input;
+	if ("post" === type ) {
+		input = "body";
+	} else if ( "get" === type ) {
+		input = "query";
 	}
 
-	// get the optional content
-	var content = req.query.content;
-	if ( !content ) {
-		content = '';
-	}
+	return function ( req, res ) {
+		// get the error code
+		var errorCode = parseInt( req[input].error, 10 );
+		if ( !errorCode ) {
+			// no code, return a splash page
+			// TODO: make this not a piece of crap
+			res.send( "Use thisserver.com/?error=XYZ, where XYZ is any valid server reponse code, and it will be returned\n" );
+			return;
+		}
 
-	// see if there's a request for a delay
-	var delay = parseInt( req.query.delay, 10 );
-	if ( delay ) {
-		setTimeout( sendData, delay );
-	} else {
-		sendData();
-	}
+		// get the optional content
+		var content = req[input].content;
+		if ( !content ) {
+			content = '';
+		}
+
+		// see if there's a request for a delay
+		var delay = parseInt( req[input].delay, 10 );
+		if ( delay ) {
+			setTimeout( sendData, delay );
+		} else {
+			sendData();
+		}
 
 
-	function sendData() {
-		res.writeHead( errorCode, {} );
-		res.end( content );
+		function sendData() {
+			res.writeHead( errorCode, {} );
+			res.end( content );
+		}
 	}
-});
+}
+
+app.get( "/", handlerMaker("get") );
+app.post( "/", handlerMaker("post") );
 
 // look at the first parameter for the port num
 var portNum = parseInt( process.argv[2], 10 ) || 8080;
